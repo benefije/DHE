2334d5b39461381e8f4b1cfe86b52a42bc890f81
benefije <jibi@ubuntu.(none)>
2014-03-10 09:25:10 +0100

    Update DHE for non blocking input

--- DHE.py
+++ DHE.py
@@ -9,8 +9,12 @@ import time
 import threading as th
 import movSword as sw
 import sharedMem
+import getch
 
-def main(robotIp, u):
+def main(robotIp):
+    gt = _Getch()
+    c = 'g'    
+    
     shm_tar = sharedMem([0,[0.,0.,0.]])
     mutex_tar = th.Lock()
     
@@ -18,14 +22,16 @@ def main(robotIp, u):
     mutex_cmd = th.Lock()
     
     visio = th.Thread(None, "TO-DO", "Visio", (shm_tar, mutex_bet, robotIp), {})
-    cmd = th.Thread(None, "TO-DO", "Commande", (shm_tar, shm_cmd, mutex_tar, mutex_cmd, robotIp), {})
+    cmd = th.Thread(None, "TO-DO", "Commande", (shm_tar, cmd, mutex_tar, mutex_cmd, robotIp), {})
     ctrl = th.Thread(None, "TO-DO", "Controle", (mcd, mutex_cmd, robotIp), {})
     
     visio.start()
     cmd.start()
     ctrl.start()
     
-    time.sleep(u)
+    while c!='s':
+        time.sleep(4.2)
+        c = gt()
     
     shm_tar.value[-2,[0.,0.,0.]]
     shm_cmd.value[-2,[0.,0.,0.,0.]]
@@ -35,14 +41,10 @@ def main(robotIp, u):
 
 if __name__ == "__main__":
     robotIp = "127.0.0.1"
-    u = 42
 
     if len(sys.argv) <= 1:
         print "Usage python almotion_changeposition.py robotIP (optional default: 127.0.0.1)"
-    elif len(sys.argv) <= 2:
-        robotIp = sys.argv[1]
     else:
         robotIp = sys.argv[1]
-        u = sys.argv[2]
 
-    main(robotIp, u)
+    main(robotIp)
